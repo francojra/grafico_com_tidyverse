a44aa5d452ab8b91527b196ac3ed48168e0198a9
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-07-04 04:34:05 -0300

    Add files via upload

--- grafico_com_tidyverse.R
+++ grafico_com_tidyverse.R
@@ -52,7 +52,7 @@ View(tabela)
 # Gráfico ----------------------------------------------------------------------------------------------------------------------------------
 
 ggplot(tabela, aes(x = sexo_biologico, y = Media)) +
-  geom_col() +
+  geom_col(fill = "#345632") +
   geom_errorbar(aes(ymin = Media - EP, ymax = Media + EP),
                 size = 0.8, width = 0.2) +
   labs(y = "Altura (cm)", x = "Sexo Biológico") +
