36e87fae99edc0e07f599ade1e6f0b545197d327
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-07-04 02:34:03 -0300

    Add files via upload

--- grafico_com_tidyverse.R
+++ grafico_com_tidyverse.R
@@ -16,4 +16,26 @@ glimpse(data)
 
 # Análise exploratória dos dados -----------------------------------------------------------------------------------------------------------
 
+## Função summary() - Média, mediana, quartis, máximos e mínimos
+
+summary(data$altura)
+summary(data$massa)
+
+# Funções describe() e describeBy() -  Média, desvio, erro, mediana, skew, kurtosis, etc. --------------------------------------------------
+
+describe(dados$N_Filhos)
+
+describeBy(dados$Salario, group = dados$Genero)
+
+describeBy(dados$Salario, group = dados$Genero:dados$Grau_de_Instruçao)
+
+# Função group_by do pacote dplyr ----------------------------------------------------------------------------------------------------------
+
+tabela <- dados %>%
+  group_by(Genero, Grau_de_Instruçao) %>%
+  summarise(Media = mean(Salario),
+            DP = sd(Salario),
+            Mediana =  median(Salario))
+tabela
+View(tabela)
 
